3cdb2cc638e675dd724444619206b932ca17561d
Eliot Reyes <eliotmij@gmail.com>
2016-08-20 13:07:22 -0700

    materialize

--- assets/javascript/user-interface.js
+++ assets/javascript/user-interface.js
@@ -1,3 +1,5 @@
+
+
 $('#game-area').fadeOut(1);
 
 var nickname;
@@ -6,8 +8,41 @@ $('#submit').on('click', function(){
   nickname = $('#nickname').val();
   $('.jumbotron').remove();
   $('#game-area').fadeIn(500);
-  var newHeader = $('<h2>');
-  newHeader.html(nickname);
-  $('#game-area').prepend(newHeader)
+
+  $('#userName').text(nickname);
+  $('#userHP').text(1000);
+
 });
 
+//Submit name on ENTER
+$(document).ready(function(){
+
+  $("#name").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#submit").click();
+    }
+  });
+
+});
+
+
+characters.splic(computerguess, 1);
+
+var characters = {
+  blackwidow : {
+    team: avengers
+  },
+
+  thor : {
+    team: avengers
+  }
+
+}
+
+charactersChosen = []
+
+for (key in characters) {
+  charactersChosen.push(key);
+}
+
+console.log(charactersChosen);
