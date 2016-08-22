f4e2e901ccad5cc6a277c477fd9dec5a87d80d90
Eliot Reyes <eliotmij@gmail.com>
2016-08-21 20:35:58 -0700

    fixed sorting, cleaned html

--- materialize/assets/js/leaderboard.js
+++ materialize/assets/js/leaderboard.js
@@ -1,26 +1,26 @@
-// database.ref("/leaderboard").update({eliot: 209});
-
 var players = {
   eliot: 789,
   john: 367,
   george: 431,
-  victor: 213
+  victor: 213, 
+  josh: 9483,
+  pierre: 273423
 }
 
 var sorted = [];
-
 
 db.ref('/players').update(players);
 
 var playersRef = db.ref('/players');
 
-playersRef.orderByValue().on('value', function(snapshot){
-  var snapLength = Object.keys(snapshot).length;
+playersRef.orderByValue().once('value').then(function(snapshot){
+  console.log(snapshot.val());
+  var snapLength = Object.keys(snapshot.val()).length -1;
   console.log(snapLength);
   snapshot.forEach(function(childSnap){
     var pname = childSnap.key;
-    var score = childSnap.val()
-    sorted.splice(snapLength, 0, { [pname] : score });
-    snapLength--;
+    var score = childSnap.val();
+    sorted.unshift({ [pname] : score });
   });
 });
+
